f6989cc71a52ee6ecc709af26ad9ba692b92cfd6
renqiuyu <renqiuyu@pualusdeMacBook-Pro.local>
2018-07-23 15:32:49 +0800

    modified 1007.cpp

--- training1-20180723/1007.cpp
+++ training1-20180723/1007.cpp
@@ -1,29 +1,61 @@
-//
-// Created by renqiuyu on 2018/7/23.
-//
 #include <iostream>
-#include <algorithm>
 #include <cstring>
 #include <cstdio>
+#include <algorithm>
 #include <cmath>
-#include <numeric>
 
 using namespace std;
+typedef long long ll;
+const double eps=1e-6;
+const ll modn = 1e9+7;
 
-
-int a[1005];
-int getsum(int n) {
-    return accumulate(a + 1, a + n + 1, 0);
+ll a[100];
+ll sa[10]={0,1,2,4,6};
+ll solve(ll n){
+    if(n<=4)
+        return sa[n];
+    ll pre=2;
+    ll ans=0;
+    for(int i=1;pre<=n;i++){
+        ans+=a[i];
+        ans%=modn;
+        pre*=2;
+    }
+    while(pre>n)
+        pre/=2;
+    n-=pre;
+    if(n==0)
+        return ans;
+    ans+=((n%modn)*((pre/2)%modn))%modn;
+    ans%=modn;
+    //cout<<"1111   "<<ans<<endl;
+    ans+=solve(n+1);
+    ans-=1;
+    ans%=modn;
+    ans+=modn;
+    ans%=modn;
+    return ans;
 
 }
+
 int main(){
-    a[1] = 1;
-    a[2] = 1;
-    for(int i = 3; i < 1005; i++) {
-        a[i] = a[i-a[i-1]] + a[i-1-a[i-2]];
+    a[1]=2;
+    a[2]=4;
+    for(int i=3;i<65;i++){
+        a[i]=a[i-1]*2;
+        a[i]%=modn;
+        ll temp=(1ll<<(i-2));
+        a[i]+=(temp%modn+1+temp%modn*2%modn)%modn*((temp/2)%modn);
+        a[i]%=modn;
+        //cout<<a[i]<<endl;
     }
-    for(int i = 2; i <= 1000; i++) {
-        cout << a[i] - a[i-1] << endl;
+
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        ll n;
+        scanf("%lld",&n);
+        cout<<solve(n)%modn<<endl;
     }
     return 0;
 }
